b710bba0e65b1f842208fed1c08fe9c8aa98a0c7
Oriol <uri.torras@gmail.com>
2012-12-13 00:39:40 +0100

    Updated npm modules bin path

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
-export PATH="./bin:$PROJECTS/ShellScripts:$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:$PROJECTS/ShellScripts:$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-export NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
+export NODE_PATH="/usr/local/lib/node_modules:/usr/local/share/npm/lib/node_modules:$NODE_PATH"
